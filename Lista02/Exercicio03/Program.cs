e4542a8d2de47b9dfda260f6ba46c316827c3dde
1164676 <1164676>
2020-02-21 17:17:03 -0300

    Primeira versão do Exercício 03 da Lista 02

---
+++ Lista02/Exercicio03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite quatro valores inteiros");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            int sp = 0, si = 0;
+            if (a % 2 == 0) sp += a; else si += a;
+            if (b % 2 == 0) sp += b; else si += b;
+            if (c % 2 == 0) sp += c; else si += c;
+            if (d % 2 == 0) sp += d; else si += d;
+            Console.WriteLine($"Soma dos pares = {sp}");
+            Console.WriteLine($"Soma dos ímpares = {si}");
+            Console.ReadKey();
+        }
+    }
+}
