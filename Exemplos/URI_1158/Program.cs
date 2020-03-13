8bf9a48f695d10c1b9028afc9da6f54bfca6a76a
1164676 <1164676>
2020-03-13 17:50:18 -0300

    Aula 12/03

---
+++ Exemplos/URI_1158/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_1158
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                int x = int.Parse(s[0]);
+                int y = int.Parse(s[1]);
+                if (x % 2 == 0) x++;
+                int soma = 0;
+                for (int k = 1; k <= y; k++)
+                {
+                    soma += x;
+                    x += 2;
+                }
+                Console.WriteLine(soma);
+            }
+            Console.ReadKey();
+        }
+    }
+}
