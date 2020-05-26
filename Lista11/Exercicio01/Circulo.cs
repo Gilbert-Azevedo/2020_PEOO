cd1ad5b932c5467e0ff4e780873ca4a43159e9fc
Gilbert Azevedo <gilbert.azevedo@academico.ifrn.edu.br>
2020-05-26 19:56:16 -0300

    Solução parcial da lista 11: Exercício 01

---
+++ Lista11/Exercicio01/Circulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio01
+{
+    // A classe Circulo modela a figura geométrica círculo e é usado para calcular
+    // sua área e circunferência. Para esses cálculos, apenas a informação do raio
+    // é necessário. Portanto, a classe vai ter um único atributo.
+    class Circulo
+    {
+        // Atributo - Por enquanto definido como público até que o conceito
+        // de encapsulamento não ser apresentado
+        public double raio;
+        // Métodos para calcular a área e circunferência
+        public double CalcArea()
+        {
+            return Math.PI * raio * raio;
+        }
+        public double CalcCircunferencia()
+        {
+            return 2 * Math.PI * raio;
+        }
+    }
+}
---
+++ Lista11/Exercicio01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Uma referência para a classe Circulo
+            Circulo c;
+            // Instancia a classe e faz a referência controlar o objeto
+            c = new Circulo();
+            // Atribui um valor para o raio
+            Console.WriteLine("Informe o raio do círculo");
+            c.raio = double.Parse(Console.ReadLine());
+            // Calcula e mostra a área e a circunferência
+            Console.WriteLine($"Área = {c.CalcArea():0.00}");
+            Console.WriteLine($"Circunferência = {c.CalcCircunferencia():0.00}");
+            Console.ReadKey();
+        }
+    }
+}
