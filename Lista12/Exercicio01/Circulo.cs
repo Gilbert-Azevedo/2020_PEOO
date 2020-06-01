676b25bf2b60208f438bee834775b28820c0a52a
Gilbert Azevedo <gilbert.azevedo@academico.ifrn.edu.br>
2020-06-01 17:02:25 -0300

    Solução parcial da lista 12: Exercício 01

---
+++ Lista12/Exercicio01/Circulo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio01
+{
+    // A classe Circulo modela a figura geométrica círculo e é usado para calcular
+    // sua área e circunferência. Para esses cálculos, apenas a informação do raio
+    // é necessário. Portanto, a classe vai ter um único atributo.
+    class Circulo
+    {
+        // Atributo - Agora encapsulado. Com isso, é possível garantir o estado
+        // dos objetos de forma que nenhum círculo tenha raio negativo.
+        private double raio;
+        // Métodos de acesso
+        public void SetRaio(double v)
+        {
+            // Os métodos set normalmente são void (não retornam valor )
+            // e testam os argumentos recebidos para garantir o estado do objeto
+            // Só armazena o raio de o valor for positivo
+            if (v > 0) raio = v;
+        }
+        public double GetRaio()
+        {
+            // Os métodos get retornam o valor de um atributo. O tipo de retorno é,
+            // portanto, igual ao tipo do atributo
+            return raio;
+        }
+        // Métodos para calcular a área e circunferência
+        public double CalcArea()
+        {
+            return Math.PI * raio * raio;
+        }
+        public double CalcCircunferencia()
+        {
+            return 2 * Math.PI * raio;
+        }
+    }
+}
---
+++ Lista12/Exercicio01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Uma referência para a classe Circulo
+            Circulo c;
+            // Instancia a classe e faz a referência controlar o objeto
+            c = new Circulo();
+            // Atribui um valor para o raio, usando o método de acesso
+            // Se um valor negativo for informado, ele não é armazenado
+            Console.WriteLine("Informe o raio do círculo");
+            c.SetRaio(double.Parse(Console.ReadLine()));
+            // Calcula e mostra a área e a circunferência
+            Console.WriteLine($"Área = {c.CalcArea():0.00}");
+            Console.WriteLine($"Circunferência = {c.CalcCircunferencia():0.00}");
+            Console.ReadKey();
+        }
+    }
+}
