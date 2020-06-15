995a6f65ba6718b565a1950910934ad6dde4e757
Gilbert Azevedo <gilbert.azevedo@academico.ifrn.edu.br>
2020-06-15 07:57:53 -0300

    Solução parcial da lista 13: Exercício 01

---
+++ Lista13/Exercicio01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entrada de dados
+            Console.WriteLine("Informe os valores iniciais de base e altura do retângulo");
+            double b = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            // Instancia a classe
+            Retangulo r = new Retangulo(b, h);
+            // Dados do objeto
+            Console.WriteLine($"Objeto = {r}");
+            Console.WriteLine($"Base = {r.GetBase()}");
+            Console.WriteLine($"Altura = {r.GetAltura()}");
+            Console.WriteLine($"Área = {r.CalcArea()}");
+            Console.WriteLine($"Diagonal = {r.CalcDiagonal()}");
+            // Novos dados
+            Console.WriteLine("Informe novos valores de base e altura");
+            r.SetBase(double.Parse(Console.ReadLine()));
+            r.SetAltura(double.Parse(Console.ReadLine()));
+            // Dados do objeto
+            Console.WriteLine($"Objeto = {r}");
+            Console.WriteLine($"Base = {r.GetBase()}");
+            Console.WriteLine($"Altura = {r.GetAltura()}");
+            Console.WriteLine($"Área = {r.CalcArea()}");
+            Console.WriteLine($"Diagonal = {r.CalcDiagonal()}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lista13/Exercicio01/Retangulo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio01
+{
+    class Retangulo
+    {
+        private double b, h;
+        // Construtor da classe. Armazena valores iniciais de base e altura se
+        // forem positivos
+        public Retangulo(double b, double h)
+        {
+            if (b > 0) this.b = b;
+            if (h > 0) this.h = h;
+        }
+        // Método de acesso. Altera valor da base se for positivo
+        public void SetBase(double b)
+        {
+            if (b > 0) this.b = b;
+        }
+        // Método de acesso. Altera valor da altura se for positivo
+        public void SetAltura(double h)
+        {
+            if (h > 0) this.h = h;
+        }
+        // Método de acesso. Retorna valor da base
+        public double GetBase()
+        {
+            return b;
+        }
+        // Método de acesso. Retorna valor da altura
+        public double GetAltura()
+        {
+            return h;
+        }
+        // Métodos de cálculo. Retornam área e diagonal, respectivamente
+        public double CalcArea()
+        {
+            return b * h;
+        }
+        public double CalcDiagonal()
+        {
+            return Math.Sqrt(b * b + h * h);
+        }
+        // Sobrescrita do método ToString
+        public override string ToString()
+        {
+            return $"Base = {b}, Altura = {h}";
+        }
+    }
+}
